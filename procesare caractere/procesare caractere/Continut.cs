71509fcecb9df103d697187e842e68f7ed60dd80
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2022-06-11 21:30:39 +0300

    Add files via upload

---
+++ procesare caractere/procesare caractere/Continut.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace procesare_caractere
+{
+    public class Continut
+    {
+        static bool IsLetter(char c)
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+        }
+        string fileName;
+        int nrlinii = 0;
+        int nrvocale = 0;
+        int nrtotalcaracter = 0;
+        int nrconsoane = 0;
+        string vocale = "aeiouAeiou";
+        public void FindFileAndWork(string fileName)
+        {                 
+                if (File.Exists(fileName))
+                {
+                    StreamReader reader = new StreamReader(fileName);
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        nrlinii++;
+                        nrtotalcaracter = nrtotalcaracter + line.Length;
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            if (IsLetter(line[i]) == true)
+                            {
+                                if (vocale.IndexOf(line[i]) != -1)
+                                {
+                                    nrvocale++;
+                                }
+                                else
+                                {
+                                    nrconsoane++;
+                                }
+
+                            }
+                        }
+                    }
+                    Console.WriteLine(nrlinii);
+                    Console.WriteLine(nrvocale);
+                    Console.WriteLine(nrconsoane);
+                    Console.WriteLine(nrtotalcaracter);
+                  
+                }
+                else
+                {
+                    Console.WriteLine("nu exista fisieru");
+                }
+            
+        }
+       
+
+    }
+}
---
+++ procesare caractere/procesare caractere/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace procesare_caractere
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var ct = new Continut();
+            string filePath = Console.ReadLine();
+            ct.FindFileAndWork(filePath);
+        }        
+            
+
+            
+        
+    }
+}
